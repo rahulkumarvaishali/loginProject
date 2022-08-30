fc00ae3a4234e2d99f4e5eef54a101b40d3f409a
Rahul Kumar <rahulrk00297@gmail.com>
2022-08-31 02:45:52 +0530

    Add project files.

---
+++ loginProject/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using loginProject.DB;
+using loginProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace loginProject.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+      
+        public ActionResult Login( loginclass obj)
+        {
+            RajaEntities entobj = new RajaEntities();
+            var result = entobj.LgnTbls.Where(a => a.Email == obj.Email).FirstOrDefault();
+            if (result == null)
+            {
+                TempData["email"] = "Invalid Email Pls Cheack Email :";
+            }
+            else
+            {
+                if (result.Email==obj.Email && result.Password == obj.Password)
+                {
+                    Session["e"]=result.Email;
+                    return RedirectToAction("Index");
+                }
+                else{
+                    TempData["pass"] = "Invalid PAssWord Or rong ";
+                }
+            }
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ loginProject/Models/loginclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace loginProject.Models
+{
+    public class loginclass
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
